dd26ce6d224d0e48d5a9a97d779fefc7f7fe6358
Vitanie <86075229+Vitaniee@users.noreply.github.com>
2023-02-09 13:08:10 +0800

    zoom out and update reset

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Xinyue Cao
  * @Date: 2023-02-08 12:12:05
- * @LastEditTime: 2023-02-08 12:32:09
+ * @LastEditTime: 2023-02-09 13:07:35
  * @LastEditors: Xinyue Cao
  */
 let homeS = 0
@@ -20,5 +20,14 @@ function increment(num,side){
         target.textContent = guestS
     }
     
+}
+
+function reset(){
+    homeS=0
+    guestS=0
+    let target = document.getElementById("homeScore")
+    target.textContent = 0
+    target = document.getElementById("guestScore")
+    target.textContent = 0
 }
 
